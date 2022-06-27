500d0c5bf49f12a6f44998f1c3ebf3708c1d397f
cepthomas <cepthomas@gmail.com>
2022-06-27 14:44:43 -0500

    WIP.

---
+++ AudioSettings.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using NAudio.Wave;
+using NBagOfTricks;
+
+
+namespace AudioLib
+{
+    [Serializable]
+    public class AudioSettings
+    {
+        /// <summary>Current audio settings. Client must set this before accessing!</summary>
+        [Browsable(false)]
+        public static AudioSettings LibSettings
+        {
+            get { if (_settings is null) throw new InvalidOperationException("Client must set this property before accessing"); return _settings; }
+            set { _settings = value; }
+        }
+        static AudioSettings? _settings = null;
+
+        #region Persisted editable properties
+        [DisplayName("Wave Output Device")]
+        [Description("How to play the audio files.")]
+        [Browsable(true)]
+        [TypeConverter(typeof(FixedListTypeConverter))]
+        public string WavOutDevice { get; set; } = "Microsoft Sound Mapper";
+
+        [DisplayName("Latency")]
+        [Description("What's the hurry?")]
+        [Browsable(true)]
+        [TypeConverter(typeof(FixedListTypeConverter))]
+        public string Latency { get; set; } = "200";
+
+        [DisplayName("Snap To Grid")]
+        [Description("Snap to msec.")]
+        [Browsable(true)]
+        public int SnapMsec { get; set; } = 100;
+        #endregion
+    }
+
+    /// <summary>Converter for selecting property value from known lists.</summary>
+    public class FixedListTypeConverter : TypeConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context) { return true; }
+        public override bool GetStandardValuesExclusive(ITypeDescriptorContext context) { return true; }
+
+        // Get the specific list based on the property name.
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            List<string>? rec = null;
+
+            switch (context.PropertyDescriptor.Name)
+            {
+                case "Latency":
+                    rec = new List<string>()
+                    {
+                        "25", "50", "100", "150", "200", "300", "400", "500"
+                    };
+                    break;
+
+                case "WavOutDevice":
+                    rec = new List<string>();
+                    for (int id = -1; id < WaveOut.DeviceCount; id++) // –1 indicates the default output device, while 0 is the first output device.
+                    {
+                        var cap = WaveOut.GetCapabilities(id);
+                        rec.Add(cap.ProductName);
+                    }
+                    break;
+            }
+
+            return new StandardValuesCollection(rec);
+        }
+    }
+}
---
+++ AudioUtils.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using NBagOfTricks;
+
+
+namespace AudioLib
+{
+    public class AudioUtils
+    {
+        /// <summary>
+        /// Export wave data to text file.
+        /// </summary>
+        /// <param name="exportFileName"></param>
+        /// <param name="rdr">Data source.</param>
+        public static void Export(string exportFileName, AudioFileReader rdr)
+        {
+            List<string> ret = new();
+            const int READ_BUFF_SIZE = 1000000;
+
+            if (rdr is not null)
+            {
+                rdr!.Position = 0; // rewind
+                var sampleChannel = new SampleChannel(rdr, false);
+
+                // Read all data.
+                long len = rdr.Length / (rdr.WaveFormat.BitsPerSample / 8);
+                var data = new float[len];
+                int offset = 0;
+                int num = -1;
+
+                while (num != 0)
+                {
+                    try // see OpenFile().
+                    {
+                        num = rdr.Read(data, offset, READ_BUFF_SIZE);
+                        offset += num;
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+                // Make a csv file of data for external processing.
+                if (sampleChannel.WaveFormat.Channels == 2) // stereo
+                {
+                    ret.Add($"Index,Left,Right");
+                    long stlen = len / 2;
+
+                    for (long i = 0; i < stlen; i++)
+                    {
+                        ret.Add($"{i + 1}, {data[i * 2]}, {data[i * 2 + 1]}");
+                    }
+                }
+                else // mono
+                {
+                    ret.Add($"Index,Val");
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        ret.Add($"{i + 1}, {data[i]}");
+                    }
+                }
+
+                File.WriteAllLines(exportFileName, ret);
+            }
+            else
+            {
+                throw new InvalidOperationException("Audio file not open");
+            }
+        }
+
+
+    }
+}
