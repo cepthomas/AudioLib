721f35dcb3a69438970ef28e6c7f25a206591950
cepthomas <cepthomas@gmail.com>
2022-10-22 11:35:27 -0500

    Fixed some scaling issues.

--- Test/Program.cs
+++ Test/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-
+using System.Windows.Forms;
 
 namespace Ephemera.AudioLib.Test
 {
@@ -11,8 +11,10 @@ namespace Ephemera.AudioLib.Test
         static void Main(string[] _)
         {
             // Use test host for debugging UI components.
-            TestHost w = new();
-            w.ShowDialog();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TestHost());
         }
     }
 }
