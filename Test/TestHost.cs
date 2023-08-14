5e9cdafa0aa4ec735f152d9d0491867910d2f14b
cepthomas <cepthomas@gmail.com>
2023-08-14 12:04:22 -0500

    WIP

--- Test/TestHost.cs
+++ Test/TestHost.cs
@@ -20,7 +20,7 @@ namespace Ephemera.AudioLib.Test
     public partial class TestHost : Form
     {
         /// <summary>Where the files are.</summary>
-        readonly string _testFilesDir = @"C:\Dev\repos\TestAudioFiles";
+        readonly string _testFilesDir = @"C:\Dev\repos\Audio\TestAudioFiles";
 
         /// <summary>The current audio provider.</summary>
         ISampleProvider _prov = new NullSampleProvider();
